de53b0acba77ee7d1ccf527770b1501edb50b848
hazdikk <m.hazdik@gmail.com>
2021-02-17 13:59:47 +0700

    edit favicon

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Hazdik Portofolio</title>
-        <link rel="icon" src  ="/favicon.ico" />
+        <link rel="icon" href={"/favicon.ico"}/>
       </Head>
 
       <main className={styles.main}>
