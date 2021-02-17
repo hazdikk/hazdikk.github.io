1812b1ead08e92484b5f412d1d2ad51f801f445f
hazdikk <m.hazdik@gmail.com>
2021-02-17 20:27:16 +0700

    fix Link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,8 +13,8 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to 
-          <Link href="/posts/first-post">
-            <a>Next.js!</a>
+          <Link href='/posts/first-post'>
+            <a>Next.js</a>
           </Link>
         </h1>
 
