a78b3f79dcaa6b43051fd50552fd3df01e8dc29b
hazdikk <m.hazdik@gmail.com>
2021-02-17 19:44:59 +0700

    move head to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Hazdik Portofolio</title>
+        <link rel="shortcut icon" href={"/favicon.ico"}/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
