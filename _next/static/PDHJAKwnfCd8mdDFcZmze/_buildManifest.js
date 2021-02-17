6f6d0e4c5661a2948b27d8020d7f1bb84e3572a6
hazdikk <33057079+hazdikk@users.noreply.github.com>
2021-02-17 06:01:22 +0000

    Deploying to gh-pages from  @ 41e5cf76517a2b5f92302001ee3c85dcff23d80f 🚀

--- _next/static/PDHJAKwnfCd8mdDFcZmze/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-83bace58f88316d726c9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9931613ce49e78964db5.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
