b1027a06bbf608dc64b819c9eb838bcfcbf79fa6
hazdikk <m.hazdik@gmail.com>
2021-02-17 19:52:50 +0700

    move head from _app to index

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
 import Head from 'next/head'
 import '../styles/globals.css'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Hazdik Portofolio</title>
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?"/>
-      </Head>
       <Component {...pageProps} />
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Hazdik Portofolio</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?"/>
+      </Head>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
