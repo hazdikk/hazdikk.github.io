7ffc6c667d39bc9363e522a526f9e6c4c21c3c3d
hazdikk <m.hazdik@gmail.com>
2021-02-17 19:30:48 +0700

    edit BASEPATH

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: '',
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/hazdikk/hazdikk.github.io/gh-pages/' : '',
 }
